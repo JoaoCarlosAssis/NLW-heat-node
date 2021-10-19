df87001da02c6f23e2551cacac43cf0a8804f93b
Joao Carlos Assis <carlos.rocha.assis@gmail.com>
2021-10-19 18:34:18 -0300

    chore: config Socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,34 @@
-import "dotenv/config"
-import express, { response }  from "express";
-import { router } from "./Routes"
+import "dotenv/config";
+import express, { response } from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+import { router } from "./Routes";
 
 const app = express();
-app.use(express.json())
+app.use(cors())
+const serverHttp = http.createServer(app)
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+})
+io.on("connection", socket => {
+  console.log(`usuário conectado no socket ${socket.id}`)
+})
+app.use(express.json());
 app.use(router);
 
-app.get('/github', (req, res) => {
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
-app.get('/signin/callback', (req, res) => {
-  const {code} = req.query;
-  return res.json(code)
-})
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+  return res.json(code);
+});
 
-app.listen(4000, ()=> console.log(`👨‍🎤 Server running on port 4000`)) 
+app.listen(4000, () => console.log(`👨‍🎤 Server running on port 4000`));
